cfa4ada058f5cf4557732df6e71d7a47266c886c
razorforse <razorforse123@gmail.com>
2018-08-05 01:46:00 +0300

    final cut

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 
-
 function close_feedback() {
     document.getElementById('feedback').style.display='none';
+    document.getElementById('header').style.opacity = '1';
+    document.getElementById('slider').style.opacity = '1';
+    document.getElementById('menu').style.opacity = '1';
+    document.getElementById('main-content').style.opacity = '1';
+    document.getElementById('footer').style.opacity = '1';
+
    }
+
+function submit() {
+	if (reqired == true) {
+	document.getElementById('feedback').style.display='none';
+    document.getElementById('header').style.opacity = '1';
+    document.getElementById('slider').style.opacity = '1';
+    document.getElementById('menu').style.opacity = '1';
+    document.getElementById('main-content').style.opacity = '1';
+    document.getElementById('footer').style.opacity = '1';
+	}
+}
